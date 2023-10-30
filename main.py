4274fbdb2ae4b9ba8694a424f3306cf70f7e28d7
adn87 <aaghadieh@gmail.com>
2023-10-30 11:43:49 -0400

    weather_data_001
    - using and understand the csv module to be able to help perform data analysis.

---
+++ main.py
@@ -0,0 +1,13 @@
+# import csv
+#
+#
+# with open("./weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+#     print(temperature)
+
+
+import pandas 
